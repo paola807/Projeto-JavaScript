2791d986d37395836d904d9e770b136f26071b7d
paola807 <00001105715127sp@al.educacao.sp.gov.br>
2024-09-06 17:20:03 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,53 +1,53 @@
 let campoIdade;
 let campoFantasia;
-let campAventura
+let campoAventura;
+
 function setup() {
   createCanvas(800, 400);
-} creanteElement("h2", "recomendador de filmes");
-createSpan("sua idade");
-campoIdade=createInput("5");
-campoFantasia=createCheckbox("gosta de fantasia?");
-campoAventura=createCheckbox("gosta de aventura");
-                            
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("Gosta de fantasia?");
+  campoAventura = createCheckbox("Gosta de aventura?");
+}
 
 function draw() {
-  background(220);
-  let idade=campoIdade.value();
-  let gostaDeFantasia=campoFantasia.checked();
-  let gostaDeAventura=campoAventura.checked();
-  let recomendacao=geraRecomendacao(idade,gostaDeFantasia,gostaDeAventura);
-  
-  fill(color(76,0,115));
-  textALign(CENTER,CENTER);
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeFantasia = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
   textSize(38);
-  text(recomendacao,width/2,height/2)
+  text(recomendacao, width / 2, height / 2);
 }
-function geraRecomedacao(idade,gostaDeFantasia ,gostaDeAventura)   {
-  if(idade>=10) {
-    if(iddade>=14)    
-       return"O menino que descobriu o vento";
-  }else{
-        if(idade>=12){
-          if (gostaDeFantasia || gostaDeAventura)
-            return "Homem aranha :no aranhaverso";
-        }else{
-      return "ladrões de bicicleta";
+
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "O menino que descobriu o vento";
+    } else {
+      if (idade >= 12) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "Homem aranha: no aranhaverso";          
+        } else{
+         return "Ladrões de bicicleta";
         }
-  } else{
-        if(gostaDeFantasia){
-        return"As aventuras de pi";
-      }else{
-        return "Depois da chuva";
+      } else {
+        if (gostaDeFantasia) {
+          return "As aventuras de pi";
+        } else {
+          return "Depois da chuva";
+        }
       }
-  }else(
-    if (gostaDeFantasia){
-    return "A viagem de chihiro";
-  }else{
-    return "O feitiço do tempo";
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "A viagem de chihiro";
+    } else {
+      return "O feitiço do tempo";
+    }
   }
 }
-}
-
-
-    
-      }
